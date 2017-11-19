dd16d78dbb64bc5c981f47210c2be918029b3fcd
gino <ginocampo@gmail.com>
2017-11-19 19:37:23 +0100

    testing

--- src/app/canvas/primocanvas/primocanvas.component.ts
+++ src/app/canvas/primocanvas/primocanvas.component.ts
@@ -26,10 +26,10 @@ export class PrimocanvasComponent implements AfterViewInit {
     }else if (this.canavasLogodisplay) {
       const canvasBandiera = this.bandiereCanvas.nativeElement ;     this.canvasBandiera = canvasBandiera;    this.contextbandiera = canvasBandiera.getContext('2d');
     }
-    this.image.src = '../../assets/mockupmagliette.png';
+    // this.image.src = '../../assets/mockupmagliette.png';
     this.image.addEventListener('load' , () => {
 
-      // this.tick();
+      this.tick();
     });
   }
 // fine ng
