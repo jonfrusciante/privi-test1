801ba5aebc323ad90f91575098fcb6d61f63ec4e
gino <ginocampo@gmail.com>
2017-11-19 16:14:36 +0100

    testing

--- src/app/canvas/primocanvas/primocanvas.component.ts
+++ src/app/canvas/primocanvas/primocanvas.component.ts
@@ -103,7 +103,7 @@ addimageinlogo(image) {
     this.testcerchio();
   //  this.roundedImage(138, 9, 137, 110, 10);
     ctx.clip();
-    ctx.drawImage(this.image1, 140, 0, 130, 118);
+    ctx.drawImage(this.image1, 142, 2, 130, 118);
     ctx.save();
   });
   this.image1.src = image ;
