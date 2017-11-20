4b6b84bd8467488eafc00953f0f9d301e3294559
gino <ginocampo@gmail.com>
2017-11-20 15:58:45 +0100

    testing

--- src/app/model/squadre.ts
+++ src/app/model/squadre.ts
@@ -1,12 +1,20 @@
+import {User} from '../user-profile/user';
+
 export interface Squadre {
-  uid?: string;
+  Uid?: string;
+  capitan_uid?: string;
   nome?: string;
   logo?: string;
+  player?: User;
+  giocatori?: User[];
   user1Uid?: string;
   user2Uid?: string;
   user3Uid?: string;
   user4Uid?: string;
-  user5Uid?: string;
+  user1Name?: string;
+  user2Name?: string;
+  user3Name?: string;
+  user4Name?: string;
 
 
 }
