7ac4c9650e4d484b3eb6ba756b1d1ac2ed897046
gino <ginocampo@gmail.com>
2017-11-20 17:10:30 +0100

    testing

--- src/app/squadre/list/list.component.ts
+++ src/app/squadre/list/list.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import {SquadreService} from '../../squadre.service';
 import {Observable} from 'rxjs/Observable';
 import {Squadre} from '../../model/squadre';
-import {User} from '../../user-profile/user';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
@@ -12,20 +11,20 @@ export class ListComponent implements OnInit {
   squdare$: Observable<Squadre[]>;
 
 
-  constructor(private squadre: SquadreService) {
-  }
+  constructor(private squadre: SquadreService) { }
 
   ngOnInit() {
     this.getsquadre();
   }
-
   getsquadre() {
     this.squdare$ = this.squadre.getsquadra();
   }
-
   rimuovisquadra(uid) {
     console.log(uid);
     this.squadre.delsquad(uid);
+
+  }
+  invitaPlayer(id){
+
   }
 }
-
