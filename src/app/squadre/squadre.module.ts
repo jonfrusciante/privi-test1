42463195ddad3e8a4b50efe9ecda5e884f534809
gino <ginocampo@gmail.com>
2017-11-20 16:09:24 +0100

    testing

--- src/app/squadre/squadre.module.ts
+++ src/app/squadre/squadre.module.ts
@@ -5,11 +5,22 @@ import { CreaComponent } from './crea/crea.component';
 import { ListComponent } from './list/list.component';
 import {RoutingRoutingModule} from './routing-routing.module';
 import { SquadreService } from '../squadre.service';
+import {
+  MatAutocompleteModule, MatButtonModule, MatCardModule, MatCheckboxModule, MatFormFieldModule, MatIconModule,
+  MatInputModule, MatMenuModule
+} from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
-    RoutingRoutingModule
+    RoutingRoutingModule, MatFormFieldModule ,
+    MatInputModule ,
+    MatButtonModule ,
+    MatCheckboxModule ,
+    MatMenuModule ,
+    MatIconModule ,
+    MatAutocompleteModule ,
+    MatCardModule ,
   ],
   declarations: [SquadreComponent, CreaComponent, ListComponent],
   providers: [SquadreService]
