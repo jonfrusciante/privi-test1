c77cfd4a4a79faf266b31200c1fe077ca27f9988
gino <ginocampo@gmail.com>
2017-11-20 12:49:31 +0100

    testing

--- src/app/squadre.service.ts
+++ src/app/squadre.service.ts
@@ -8,7 +8,7 @@ import {AngularFirestore} from 'angularfire2/firestore';
 @Injectable()
 export class SquadreService {
    user$: Observable<User>;
-   squadra$: Observable<Squadre[]>;
+   squadra$: Observable<Array<Squadre>>;
 
   constructor(private user: AuthService, private afs: AngularFirestore) {
     this.user$ = this.user.user;
