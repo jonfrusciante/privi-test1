8aed4921af76047a1691796e14faa55a2ee511ac
gino <ginocampo@gmail.com>
2017-11-20 16:51:16 +0100

    testing

--- src/app/squadre/list/list.component.ts
+++ src/app/squadre/list/list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {SquadreService} from '../../squadre.service';
 import {Observable} from 'rxjs/Observable';
 import {Squadre} from '../../model/squadre';
+import {User} from '../../user-profile/user';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
@@ -26,5 +27,18 @@ export class ListComponent implements OnInit {
   }
   invitaPlayer(id){
 
+  }
+  rimuovigiocatore(giocatori, scuadra: Squadre) {
+    this.squadre.squadraCollection$.doc(scuadra.Uid).snapshotChanges().subscribe(
+      result => {
+        const squadra = scuadra;
+        const user = result.payload.data() as User;
+        const filtered =  squadra.giocatori.filter(function(el) { return el.uid !== user.uid ; });
+        console.log(filtered);
+        squadra.giocatori = filtered;
+        console.log(squadra)
+        // this.ElimSquadre(squadra);
+      }
+    );
   }
 }
