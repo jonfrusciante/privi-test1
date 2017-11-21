a62c15fb17914f42bbe39d533cbc9f2e63fe1ba6
gino <ginocampo@gmail.com>
2017-11-21 14:41:10 +0100

    Revert "testing"
    This reverts commit 0ef3021

--- src/app/squadre/crea/crea.component.ts
+++ src/app/squadre/crea/crea.component.ts
@@ -25,6 +25,7 @@ interface Squadre {
   styleUrls: ['./crea.component.css']
 })
 export class CreaComponent implements OnInit {
+  @ViewChild(PrimocanvasComponent) canavas: PrimocanvasComponent;
   private SqudreCollections: AngularFirestoreCollection<Squadre>;
   private UserCollection: AngularFirestoreCollection<User>;
   myForm: FormGroup;
@@ -37,14 +38,89 @@ export class CreaComponent implements OnInit {
     {value: 'juve', nome: 'JUVENTUS'},
     {value: 'inter', nome: 'INTER'},
   ];
-  constructor() {
-
+  constructor(private usrServ: UserService, private afs: AngularFirestore, private user: AuthService) {
+    this.SqudreCollections = this.afs.collection<Squadre>('squadre_create');
+    this.UserCollection = this.afs.collection<User>('users');
 
   }
+  updatesquadracuore(squadra) {
+    if (squadra.value === 'juve') {
+      console.log(squadra.value);
+      this.canavas.juve();
+    }else if (squadra.value === 'inter') {
+      console.log(squadra.value);
 
+      this.canavas.inter();
+    }else if (squadra.value === 'milan'  ){
+      console.log(squadra.value);
+
+      this.canavas.milan();
+    }   // console.log(squadra.value);
+
+  }
+  updatetesto(testo) {
+    console.log(testo.target.value);
+    this.canavas.inseriscitesto(testo.target.value);
+  }
   ngOnInit() {
+    this.myForm = new FormGroup({
+      squadraCuore: new FormControl(),
+      nome: new FormControl() ,
+      //  logo: new FormControl({value: this.fileImage, disabled: false}),
+      //    userid: new FormControl(''),
+    });
+
+  }
+  setSquadra(squadra: Squadre) {
+
+    this.SqudreCollections.add(squadra).then(value => {
+      this.user.user.subscribe(user => {
+        squadra.Uid = value.id;
+        squadra.capitan_uid = user.uid;
+        this.SqudreCollections.doc(value.id).update(squadra);
+        console.log(value.id);
+        this.addSquadraUser(squadra);
+      });
+
+    });
+
+  }
+  addSquadraUser(squadra: Squadre) {
+    this.UserCollection.doc(squadra.capitan_uid).collection('squadre').doc(squadra.Uid).set(squadra);
+  }
+  onSubmit(form: FormGroup) {
+    console.log(this.canavas.getfileimage());
+    this.team = {nome: form.value.nome.toString(), logo: this.canavas.getfileimage(), squadracuore: form.value.squadraCuore.toString()};
+    this.team.giocatori = [];
+    this.setSquadra(this.team);
+    console.log( this.team);
+    this.myForm.reset();
+//    this.cleanform();
+  }
+
+  private cleanform() {
+    this.myForm.controls['logo'].setValue('');
+    this.myForm.controls['nome'].setValue('');
+    this.myForm.controls['squadraCuore'].setValue('');
 
 
   }
+  fileChange($event) {
+    const reader = new FileReader();
+    if ($event.target.files && $event.target.files.length > 0) {
+      const file = $event.target.files[0];
+      reader.readAsDataURL(file);
+      reader.onload = () => {
 
+        // FAI QUALCOSA CON IL FILE PRONTO
+
+        // this.canavas.nextpass(reader.result);
+        // this.myForm.controls['logo'].setValue(reader.result.split(',')[1]);
+        // chiamata ad imgurur!! per uplodare la foto e ricevere il link indietro
+        // this.usrServ.getauttoken(reader.result.split(',')[1]).subscribe( result => {
+        //   this.myForm.controls['logo'].setValue(result.data.link);
+        // this.fileImage = result.data.link ; });
+      };
+    }
+  }
 }
