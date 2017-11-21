2e3277869d45937e9c649796ea03d646e3598d7a
gino <ginocampo@gmail.com>
2017-11-21 14:34:07 +0100

    Revert "testing"
    This reverts commit 0ef3021

--- src/app/canvas/appcanvas.module.ts
+++ src/app/canvas/appcanvas.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {PrimocanvasComponent} from './primocanvas/primocanvas.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-
-  ],
+  imports: [ CommonModule ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   declarations: [PrimocanvasComponent
-  ]
+  ],
+  exports: [ PrimocanvasComponent ],
+
 })
 export class AppcanvasModule {
 
