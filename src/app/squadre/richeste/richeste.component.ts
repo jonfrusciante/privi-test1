6df198ad4730cd0e0bd9767236fd2fd296917a5b
gino <ginocampo@gmail.com>
2017-11-26 16:38:11 +0100

    reface auth and user sevice try out!!!

--- src/app/squadre/richeste/richeste.component.ts
+++ src/app/squadre/richeste/richeste.component.ts
@@ -42,13 +42,14 @@ export class RichesteComponent implements OnInit {
   getuser(): Observable<RichiesteIn[]> {
      return this.richestin$.map(
       value => {
+        console.log('kkk');
+
         return value.map(
           res => {
-            console.log('kkk')
               const a: AngularFirestoreDocument<User> =  this.afs.collection('users').doc(res.userhomeid);
               return   a.valueChanges().map(
                 us => { return res.dataUser = us ;}
-              ).subscribe();
+              )
           }
         );
       }
