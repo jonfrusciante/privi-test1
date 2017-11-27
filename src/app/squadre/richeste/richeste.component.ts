bed1e6003a37cc41d795a56c1dfb7857c1835013
gino <ginocampo@gmail.com>
2017-11-27 18:22:16 +0100

    reface auth and user sevice try out!!!

--- src/app/squadre/richeste/richeste.component.ts
+++ src/app/squadre/richeste/richeste.component.ts
@@ -67,13 +67,16 @@ export class RichesteComponent implements OnInit {
   getRichesteIn(){
   return this.userR.user.switchMap(
     (user) => {
-      return this.afs.collection('users').doc(user.uid).collection<RichiesteIn>('richesteIn', ref => ref.where('confermato', '==', false)).valueChanges().switchMap(
+      return this.afs.collection('users').doc(user.uid).collection<RichiesteIn>('richesteIn', ref => ref.where('confermato', '==', false)).valueChanges()
+        .map(
         (ric) => {
          return ric.map((rr) => {
            return this.afs.collection('users').doc(rr.userhomeid).snapshotChanges().map(
              (g) => {
+               const h = [];
                rr.dataUser = g.payload.data() as User ;
-               return ric ;
+               h.push(rr);
+               return h ;
              }
            );
          });
@@ -82,7 +85,7 @@ export class RichesteComponent implements OnInit {
     });
 }
   ngOnInit() {
-    this.richestinc$ = this.getRichesteIn()
+    this.richestinc$ = this.getRichesteIn();
     this.richestinc$.subscribe(
       aaa => console.log(aaa)
     )
