33a3464729c3c5a8d7219547e1aee76b4f0e70b9
gino <ginocampo@gmail.com>
2017-12-03 19:19:40 +0100

    reface auth and user sevice try out!!!

--- src/app/squadre/richeste/richeste.component.ts
+++ src/app/squadre/richeste/richeste.component.ts
@@ -159,8 +159,7 @@ console.log(arr);    return arr;
 
     const h = function () {
       for (let obj of datiprenotazione.user) {
-        obj.subscribe(r => p.push(r));
-        console.log(p);
+         console.log(obj);
       }
     };
     h();
