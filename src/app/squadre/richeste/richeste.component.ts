dc07c63df7df6ac4288c5837c05831049edb5f16
gino <ginocampo@gmail.com>
2017-12-03 19:08:37 +0100

    reface auth and user sevice try out!!!

--- src/app/squadre/richeste/richeste.component.ts
+++ src/app/squadre/richeste/richeste.component.ts
@@ -155,7 +155,10 @@ console.log(arr);    return arr;
     console.log(item);
   }
   sendwhatup(number, datiprenotazione: Richieste, user ) {
-    console.log(number , datiprenotazione, user);
+    const gioc = datiprenotazione.user.map(
+      r => r.map( re => re.displayName)
+    );
+    console.log(number , datiprenotazione, gioc);
 
     const testo = {data: format(datiprenotazione.dataid, 'DD-MM-YYYY'), ora: datiprenotazione.oraid , giocatori: datiprenotazione.user}
     const testodainviare = 'Salve hai un invito per la partita chr si terra i data ' + testo.data +  'alle ore' + testo.ora + 'organizzata da '  + datiprenotazione.masteruser + '.' +  'I giocatori invitati sono  :' ;
