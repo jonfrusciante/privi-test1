cdbfd2e63552e37a8a9f5a6aa27ade5af161437d
gino <ginocampo@gmail.com>
2017-12-05 00:14:29 +0100

    reface auth and user sevice try out!!!

--- src/app/squadre/richeste/richeste.component.ts
+++ src/app/squadre/richeste/richeste.component.ts
@@ -8,6 +8,7 @@ import {AuthService} from '../../core/auth.service';
 import 'rxjs/add/operator/map';
 import {format} from 'date-fns';
 import {WathsupService} from '../../wathsup.service';
+import 'rxjs/add/operator/concatAll';
 
 
 
@@ -152,7 +153,7 @@ console.log(arr);    return arr;
   }
   sendwhatup(number, datiprenotazione: Richieste, user ) {
 
-    const c = Observable.of(...datiprenotazione.user).combineAll() ;// .map( (flat) => flat).subscribe( (aa) => console.log(aa)) ; // .map(( f: User) => f.displayName ).subscribe( hh => console.log(hh));
+    const c = Observable.of(...datiprenotazione.user).concatAll() ;// .map( (flat) => flat).subscribe( (aa) => console.log(aa)) ; // .map(( f: User) => f.displayName ).subscribe( hh => console.log(hh));
 
     c.subscribe(ff => console.log(ff));
     // console.log(c);
