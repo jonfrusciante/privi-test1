5a4ec80b56a99c30cc984123f9fdeda46caa2f14
gino <ginocampo@gmail.com>
2017-12-04 17:08:13 +0100

    reface auth and user sevice try out!!!

--- src/app/squadre/richeste/richeste.component.ts
+++ src/app/squadre/richeste/richeste.component.ts
@@ -158,7 +158,7 @@ console.log(arr);    return arr;
   }
   sendwhatup(number, datiprenotazione: Richieste, user ) {
      console.log (datiprenotazione.user);
-    const c = Observable.concat(datiprenotazione.user);
+    const c = Observable.merge(datiprenotazione.user);
     c.subscribe(f => console.log(f));
     const testo = {data: format(datiprenotazione.dataid, 'DD-MM-YYYY'), ora: datiprenotazione.oraid , giocatori: datiprenotazione.user}
     const testodainviare = 'Salve hai un invito per la partita chr si terra i data ' + testo.data +  'alle ore' + testo.ora + 'organizzata da '  + datiprenotazione.masteruser + '.' +  'I giocatori invitati sono  :' ;
