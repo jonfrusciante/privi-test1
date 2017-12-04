1e778af9c3852d49a0f751235329836b6ce94e9f
gino <ginocampo@gmail.com>
2017-12-04 23:46:05 +0100

    reface auth and user sevice try out!!!

--- src/app/squadre/richeste/richeste.component.ts
+++ src/app/squadre/richeste/richeste.component.ts
@@ -159,7 +159,7 @@ console.log(arr);    return arr;
   sendwhatup(number, datiprenotazione: Richieste, user ) {
      const test=[];
     for (let obj: Observable<User> of datiprenotazione.user) {
-     obj.subscribe(dd => test.push(dd));
+     obj.subscribe(c => console.log(c));
     }
     console.log(test);
     // Observable.merge(...datiprenotazione.user , c) ;// .map( (flat) => flat).subscribe( (aa) => console.log(aa)) ; // .map(( f: User) => f.displayName ).subscribe( hh => console.log(hh));
