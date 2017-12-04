2413a2e1f6e7964f5ed420df0f9be7706a2da223
gino <ginocampo@gmail.com>
2017-12-05 00:05:12 +0100

    reface auth and user sevice try out!!!

--- src/app/squadre/richeste/richeste.component.ts
+++ src/app/squadre/richeste/richeste.component.ts
@@ -51,7 +51,7 @@ interface Richieste {
   dataid?: string;
   oraid?: string;
   masteruser?: string;
-  user: any[];
+  user: Observable<User>[];
 }
 @Component({
   selector: 'app-richeste',
@@ -158,7 +158,7 @@ console.log(arr);    return arr;
   }
   sendwhatup(number, datiprenotazione: Richieste, user ) {
 
-    Observable.forkJoin(...datiprenotazione.user).combineAll().subscribe(a => console.log(a)) ;// .map( (flat) => flat).subscribe( (aa) => console.log(aa)) ; // .map(( f: User) => f.displayName ).subscribe( hh => console.log(hh));
+    Observable.merge(...datiprenotazione.user).subscribe(a => console.log(a)) ;// .map( (flat) => flat).subscribe( (aa) => console.log(aa)) ; // .map(( f: User) => f.displayName ).subscribe( hh => console.log(hh));
     // console.log(c);
     const testo = {data: format(datiprenotazione.dataid, 'DD-MM-YYYY'), ora: datiprenotazione.oraid , giocatori: datiprenotazione.user}
     const testodainviare = 'Salve hai un invito per la partita chr si terra i data ' + testo.data +  'alle ore' + testo.ora + 'organizzata da '  + datiprenotazione.masteruser + '.' +  'I giocatori invitati sono  :' ;
