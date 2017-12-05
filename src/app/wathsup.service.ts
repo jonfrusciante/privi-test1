460eccc25c41e3952431f1539ff601f3b00fcb34
gino <ginocampo@gmail.com>
2017-12-05 17:53:57 +0100

    reface auth and user sevice try out!!!

--- src/app/wathsup.service.ts
+++ src/app/wathsup.service.ts
@@ -11,13 +11,13 @@ private readonly url = 'https://www.waboxapp.com/api/send/chat';
  sendmessage(number, testo): Observable<any> {
    const myHeaders = new Headers();
    myHeaders.append('Content-Type', 'application/x-www-form-urlencoded');
-   const search = {
-   token : '30c58c744a12ef74b365686b243bec9a5a240c87e974e',
-   uid : '393200771189' ,
-   to : number,
-   text: testo };
-   // let options = new RequestOptions({ headers: myHeaders, params: search });
-   console.log(this.url , search);
-   return this.http.post( this.url, search).map( r =>  r.json()  );
+   const search = new URLSearchParams();
+   search.set('token', '30c58c744a12ef74b365686b243bec9a5a240c87e974e');
+   search.set('uid', '393200771189');
+   search.set('to', number);
+   search.set('text', testo);
+   const options = new RequestOptions({ headers: myHeaders, params: search });
+
+   return this.http.post( this.url, {}, options).map( r =>  r.json()  );
  }
 }
