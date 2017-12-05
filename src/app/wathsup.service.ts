3ba79df22d35b0b92f8cb7ef7df69e571e83048a
gino <ginocampo@gmail.com>
2017-12-05 21:38:28 +0100

    reface auth and user sevice try out!!!

--- src/app/wathsup.service.ts
+++ src/app/wathsup.service.ts
@@ -16,7 +16,7 @@ private readonly url = 'https://www.waboxapp.com/api/send/chat';
    search.set('text', testo);
 
    return this.http.post( this.url, {}, {
-     params: new HttpParams().set('token', '30c58c744a12ef74b365686b243bec9a5a240c87e974e').set('uid', '393200771189').set('to', number).set('text', testo).set('custom_uid', 'test' + (Math.random() * 100) )
+     params: new HttpParams().set('token', '30c58c744a12ef74b365686b243bec9a5a240c87e974e').set('uid', '393200771189').set('to', number).set('text', testo).set('custom_uid', 'test' + Math.floor((Math.random() * 1000)) )
    });
  }
 }
