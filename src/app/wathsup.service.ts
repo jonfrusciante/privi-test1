601e5e2aac52cdc4bfbe800a44bbd7d01e75e72a
gino <ginocampo@gmail.com>
2017-12-05 19:19:04 +0100

    reface auth and user sevice try out!!!

--- src/app/wathsup.service.ts
+++ src/app/wathsup.service.ts
@@ -1,25 +1,21 @@
 import { Injectable } from '@angular/core';
-import { Http, Response, RequestOptions, Headers } from '@angular/http';
 import {Observable} from 'rxjs/Observable';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable()
 export class WathsupService {
 private readonly url = 'https://www.waboxapp.com/api/send/chat';
-  constructor(private http: Http) {
+  constructor(private http: HttpClient) {
 
   }
  sendmessage(number, testo): Observable<any> {
-   const myHeaders = new Headers();
-   myHeaders.append('Content-Type', 'application/json');
-   myHeaders.append(    'Access-Control-Allow-Origin', 'https://www.waboxapp.com/' );
+   const search = {
+     token : '30c58c744a12ef74b365686b243bec9a5a240c87e974e',
+     uid : '393200771189' ,
+     to : number,
+     text: testo };
+   // const options = new RequestOptions({ headers: myHeaders, params: search });
 
-   const search = new URLSearchParams();
-   search.set('token', '30c58c744a12ef74b365686b243bec9a5a240c87e974e');
-   search.set('uid', '393200771189');
-   search.set('to', number);
-   search.set('text', testo);
-   const options = new RequestOptions({ headers: myHeaders, params: search });
-
-   return this.http.post( this.url, {}, options).map( r =>  r.json()  );
+   return this.http.post( this.url, search).map( r =>  r  );
  }
 }
