3eed38e34b52faa9f6990cb7366c080ee8661e98
gino <ginocampo@gmail.com>
2017-12-05 17:38:37 +0100

    reface auth and user sevice try out!!!

--- src/app/squadre/richeste/richeste.component.ts
+++ src/app/squadre/richeste/richeste.component.ts
@@ -185,6 +185,6 @@ console.log(arr);    return arr;
    // this.wz.sendmessage(number,testodainviare);asaas
     const testo = {data: format(datiprenotazione.dataid, 'DD-MM-YYYY'), ora: datiprenotazione.oraid , giocatori: datiprenotazione.user};
     const testodainviare = 'Salve hai un invito per la partita chr si terra i data ' + testo.data +  'alle ore' + testo.ora + 'organizzata da '  + datiprenotazione.masteruser + '.' +  'I giocatori invitati sono  :' + userA ;
-    this.wz.sendmessage(number, testodainviare);
+    this.wz.sendmessage(number, testodainviare).subscribe();
   }
 }
