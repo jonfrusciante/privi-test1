23b11e5efef3b4d2927769df75cea43805d25774
gino <ginocampo@gmail.com>
2017-12-05 17:15:12 +0100

    reface auth and user sevice try out!!!

--- src/app/squadre/richeste/richeste.component.ts
+++ src/app/squadre/richeste/richeste.component.ts
@@ -181,7 +181,7 @@ console.log(arr);    return arr;
      return testodainviare ;
    }).map(x => {
      return this.wz.sendmessage(number, x);
-   }).subscribe();
+   }).takeLast(2).subscribe();
 
    // this.wz.sendmessage(number,testodainviare);asaas
   }
