1929bf011bcc6cb40be6755831b25a71ea602b16
gino <ginocampo@gmail.com>
2017-12-09 19:07:05 +0100

    try aws

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAzZz1QrOW-IUyCVGc-lkGUZIoxxoPkzlU',
+    authDomain: 'valguarnera-bdf92.firebaseapp.com',
+    databaseURL: 'https://valguarnera-bdf92.firebaseio.com',
+    projectId: 'valguarnera-bdf92',
+    storageBucket: 'valguarnera-bdf92.appspot.com',
+    messagingSenderId: '222559086438'
+  }
 };
